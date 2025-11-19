674eecc0445a072dd9258edf29830ba39eb546ac
Riyat121 <145189781+Riyat121@users.noreply.github.com>
2025-11-20 02:15:08 +0530

    added photo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import plant from "../assets/plant.png"
+import search from "../assets/search.png"
+import bag from "../assets/bag.png"
 function Navbar() {
   return (
     <div className='p-8 flex '>
-      <img className='h-10 w-10 ' src="src\assets\plant.png" alt="" />
+      <img className='h-10 w-10 ' src={plant} alt="" />
       <h2 className='font-bold text-2xl text-amber-50'>FloraVision.</h2>
       <div className='items-center ml-90'>
         <ul className='flex font-(family-name:Indie_Flower) text-[#FFFFFF] justify-center gap-5 '>
@@ -14,8 +16,8 @@ function Navbar() {
         </ul>
       </div>
       <div className='flex ml-110 gap-6'>
-        <img className='h-4 w-4 ' src="src\assets\search.png" alt="" />
-        <img  className='h-4 w-4 ' src="src\assets\bag.png" alt="" />
+        <img className='h-4 w-4 ' src={search} alt="" />
+        <img  className='h-4 w-4 ' src={bag} alt="" />
         <div className="flex flex-col gap-1 ml-6">
   <div className="w-6 h-1 rounded-2xl  bg-white"></div>
   <div className="w-3 h-1 ml-3 rounded-2xl  bg-white"></div>
